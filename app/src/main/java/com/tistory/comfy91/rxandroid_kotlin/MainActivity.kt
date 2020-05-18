a5f86da18f9bf2a8cb10f19375b705e7ccd1a65d
kshvv975@gmail.com <RoadtoS7@users.noreply.github.com>
2020-05-18 20:01:22 +0900

    Initial commit

---
+++ app/src/main/java/com/tistory/comfy91/rxandroid_kotlin/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.tistory.comfy91.rxandroid_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * HelloWorld 출력
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
