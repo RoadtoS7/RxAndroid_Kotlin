307656ca9b2e875adbb4dc1491b866c52564c4ea
kshvv975@gmail.com <RoadtoS7@users.noreply.github.com>
2020-05-18 20:56:24 +0900

    Hello2Activity: HelloWorld 띄우는 것 lambda 표현식으로 바꿈

--- app/src/main/java/com/tistory/comfy91/rxandroid_kotlin/Hello2Activity.kt
+++ app/src/main/java/com/tistory/comfy91/rxandroid_kotlin/Hello2Activity.kt
@@ -2,11 +2,34 @@ package com.tistory.comfy91.rxandroid_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableObserver
+import kotlinx.android.synthetic.main.activity_hello2.*
 
 class Hello2Activity : AppCompatActivity() {
+    private val TAG = Hello2Activity::class.simpleName
+
+    private lateinit var mDisposable: Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello2)
+
+        Observable.create<String>{
+            it.onNext("Hello World!")
+            it.onComplete()
+        }.subscribe{o -> textView.text = o}
+
+        // just 사용
+//        Observable.just("Hello World!")
+//            .subscribe(textView::setText)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(!mDisposable.isDisposed){
+            mDisposable.dispose()
+        }
     }
 }
---
+++ app/src/main/java/com/tistory/comfy91/rxandroid_kotlin/LoopActivity.kt
@@ -0,0 +1,12 @@
+package com.tistory.comfy91.rxandroid_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoopActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loop)
+    }
+}
